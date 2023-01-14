4b807304ec1602e33b0e1f2ddc8e0dc1e52679d2
uttamkrshaw <uttamkr5599@gmail.com>
2023-01-14 17:30:44 +0530

    push

---
+++ index.js
@@ -0,0 +1,18 @@
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
+
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
